52793f02318dad92843834c5fa60f09b9b7d6ca4
Phil McClure <pmcclure@rumblelabs.com>
2011-09-27 15:07:31 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestCapWebhook::Application.config.secret_token = 'e6c82888806ce77cbcd8b8556105036f861f7089f1586d633bdfbbe41f7a8b7f62cdf2fa0636d182187d3169161fcc5200fa4cc65f999caf4c2122f998c562a9'
